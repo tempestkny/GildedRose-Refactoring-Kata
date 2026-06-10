dfdfe968e2b8bf67e1f9be808911b1e485a6d88f
tempestkny <kenan.sabic@live.at>
2026-06-10 19:10:29 +0200

    test: cover regualr item degradation after sell date

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -15,4 +15,14 @@ public class GildedRoseTest
         Assert.Equal(9, Items[0].SellIn);
         Assert.Equal(9, Items[0].Quality);
     }
+
+    [Fact]
+    public void RegularItem_DecreasesQualityByTwoAfterSellByDate()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Regular Item", SellIn = 0, Quality = 10 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(-1, Items[0].SellIn);
+        Assert.Equal(8, Items[0].Quality);
+    }
 }
